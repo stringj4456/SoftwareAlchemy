7ee9669946923279d2d5d59b0379008e3ffd50bf
StingJ <justinsaechao23@gmail.com>
2025-05-14 19:25:58 -0700

    Simple rot13 done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SoftwareAlchemy
+This repository is a playground for all of my little projects and an opportunity for me to build my programming skills
--- rot13.py
+++ rot13.py
@@ -1,43 +1,38 @@
 """
-Simple ROT13 Encoder. For this first instance, only the first character can be
-successfully shifted. Uses 1 based indexing not 0 based.
+Simple ROT13 Encoder. Uses 1 based indexing instead of 0 based. Only supports lowercase letters
+currently. Uppercase letters to be added eventually for more complexity.
 """
 
 #Alphabet character list
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+#Shifted letter array
+shifted = []
+
 #Get the plaintext string from the user
 plaintext = list(input("Enter the string to rotate: "))
 print()
-first = plaintext[0]
 
-#Find the first letter of the plaintext in the alphabet list
-for index,value in enumerate(alphabet):
-    if first == value:
-        found = index
-        break
+#Loop through the alphabet list for each plaintext list character
+for letter in plaintext:
 
-#Shift the found index 13 places
-shift_index = found + 13
+    #Find the letter and store its index
+    for index, value in enumerate(alphabet):
+        if letter == value:
+            found = index
+            break
 
-#Check if the new index exceeds the alphabet list size
-if shift_index > 25:
-    cycle_index = shift_index - len(alphabet)
-    shifted = alphabet[cycle_index]
+    #Shift the found index 13 places
+    shift_index = found + 13
 
-    #Test prints
-    print(f"Original Value: {first}")
-    print(f"Original Index: {found}")
-    print()
-    print(f"Shifted Index: {cycle_index}")
-    print(f"Shifted Value: {shifted}")
-else:
-    shifted2 = alphabet[shift_index]
+    #Check if the new index exceeds the alphabet list size
+    if shift_index > 25:
+        cycle_index = shift_index - len(alphabet)
+        shifted.append(alphabet[cycle_index])
+    else:
+        shifted.append(alphabet[shift_index])
 
-    #Test prints
-    print(f"Original Value: {first}")
-    print(f"Original Index: {found}")
-    print()
-    print(f"Shifted Index: {shift_index}")
-    print(f"Shifted Value: {shifted2}")
+#Join and print the shifted letters
+output = "".join(shifted)
+print(f"Your shifted string is: {output}")
