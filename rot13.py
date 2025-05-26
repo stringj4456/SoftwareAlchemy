ccf5b6667a4c8dc67b56c05940ca14f50769b0d5
StingJ <justinsaechao23@gmail.com>
2025-05-26 15:41:46 -0700

    Added space support

--- rot13.py
+++ rot13.py
@@ -1,6 +1,6 @@
 """
 Simple ROT13 Encoder. Uses 1 based indexing instead of 0 based. Only supports lowercase letters
-currently. Uppercase letters to be added eventually for more complexity.
+currently. Now supports spaces.
 """
 
 #Alphabet character list
@@ -14,12 +14,19 @@ shifted = []
 plaintext = list(input("Enter the string to rotate: "))
 print()
 
+#Plaintext index
+plaintext_i = 0
+
 #Loop through the alphabet list for each plaintext list character
 for letter in plaintext:
-
+    if letter == " ":           #If a space is encountered
+        shifted.append(plaintext[plaintext_i])
+        plaintext_i += 1
+        continue                #End this iteration and goto the next
+        
     #Find the letter and store its index
     for index, value in enumerate(alphabet):
-        if letter == value:
+        if letter == value:     #If a match is found
             found = index
             break
 
@@ -32,6 +39,8 @@ for letter in plaintext:
         shifted.append(alphabet[cycle_index])
     else:
         shifted.append(alphabet[shift_index])
+
+    plaintext_i += 1    #Increment plaintext index
 
 #Join and print the shifted letters
 output = "".join(shifted)
