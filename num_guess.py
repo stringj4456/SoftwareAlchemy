ee47faf696eb3ceecdad67d870cf31fa7afb8a4b
StingJ <justinsaechao23@gmail.com>
2025-06-02 17:53:31 -0700

    Added attempt decrementer

--- num_guess.py
+++ num_guess.py
@@ -1,24 +1,37 @@
-#Number guessing game. Guess a number between 1-10. Simple implementation
+#Number guessing game. Guess a number between 1-10.
 
 import random
 
 found = False                           #Game condition
 target = random.randint(1, 10)          #Generate a random number
 num_guesses = 0                         #Number of guesses
+attempts = 3                            #Number of attempts
+sub_attempt = attempts                  #Count down for attempts
 
 #Main program loop
 while found == False:
-    print()
-    user_guess = int(input("Enter a guess: "))   #Get the users guess
+    #Check number of attempts
+    if num_guesses == attempts:
+        print()
+        print("Sorry but you've ran out of guesses.")
+        print("Better luck next time!")
+        exit()
+
+    #Get the users guess
+    user_guess = int(input("Enter a guess: "))
     print()
 
-    if user_guess == target:                     #If the guess is correct
+    #Increment/decrement number of guesses/attempts
+    num_guesses += 1 
+    sub_attempt -= 1 
+
+    #Guess conditions
+    if user_guess == target:
         print("You guessed correctly!")
+        print("Nice job!")
         found = True
-    else:                                        #If the guess is wrong
+    else:
         print("Incorrect.")
-
-    num_guesses += 1                             #Increment guess count
-    print()
-    print(f"Number of guesses: {num_guesses}")
-
+        if sub_attempt != 0:
+            print(f"You have {sub_attempt} attempts left.")
+            print()
