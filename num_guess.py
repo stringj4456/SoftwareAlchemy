1a0d83f22228bdad0d2dad5510b5bbd4200b0041
StingJ <justinsaechao23@gmail.com>
2025-06-02 17:28:37 -0700

    Added guess counter

--- num_guess.py
+++ num_guess.py
@@ -4,16 +4,21 @@ import random
 
 found = False                           #Game condition
 target = random.randint(1, 10)          #Generate a random number
+num_guesses = 0                         #Number of guesses
 
 #Main program loop
 while found == False:
     print()
-    guess = int(input("Enter a guess: "))   #Get the users guess
+    user_guess = int(input("Enter a guess: "))   #Get the users guess
     print()
 
-    if guess == target:                     #If the guess is correct
+    if user_guess == target:                     #If the guess is correct
         print("You guessed correctly!")
         found = True
-    else:                                   #If the guess is wrong
+    else:                                        #If the guess is wrong
         print("Incorrect.")
+
+    num_guesses += 1                             #Increment guess count
+    print()
+    print(f"Number of guesses: {num_guesses}")
 
