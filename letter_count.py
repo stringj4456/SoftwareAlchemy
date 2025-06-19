a38b6f77fc2609d1999d59a01a2cf296598b7ab8
StingJ <justinsaechao23@gmail.com>
2025-06-18 19:48:22 -0700

    Progress 2

--- letter_count.py
+++ letter_count.py
@@ -2,29 +2,41 @@
 
 #Variables
 char = ""
-stored_char = []
 stored_count = []
 found_char = []
 
 main_i = 0
 char_count = 0
-inner_i = 0
+count = 0
 
-#User inputs
+#Letter count function
+def count_letter(count, char):
+    char_count = 0
+    i = 0
+
+    while i != len(string):
+        if char in string:
+            char_count += 1
+        i += 1
+    return char_count
+
 string = input("Enter the string: ")
 
 #Loop through the users string and count letter occurrences
 while main_i != len(string):
 
     char = string[main_i]
-    stored_char.append(char)
+   
+    #Get the current letter of the string
+    if char in found_char:
+        main_i += 1
+        continue
+    else:
+        found_char.append(char)
+    
+    #Count number of occurrences for the letter
+    # char_count = count_letter(count, char)
+    # stored_count.append(char_count)
 
-    while inner_i != len(string):
-        if char in string:
-            char_count += 1
-        inner_i += 1
-
-    print(char_count)
     main_i += 1
-
-print(stored_char)
+print(found_char)
