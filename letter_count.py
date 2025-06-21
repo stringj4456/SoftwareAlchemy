bb2dd63730a978e0b445018af35128e9bc734908
StingJ <justinsaechao23@gmail.com>
2025-06-20 20:44:53 -0700

    Finished. More to add, but core done

--- letter_count.py
+++ letter_count.py
@@ -1,42 +1,56 @@
 #Letter counter. Takes in a string and will count the number of occurrences for each letter
 
-#Variables
-char = ""
-stored_count = []
-found_char = []
+#The program works by cycling through the entered string character by character. If a character has not yet been encountered,
+#the program will store that letter in found_char for future reference. If encountered again, the program will skip the next 
+#occurrences of the letter. This ensures each unique letter is handled only once.
 
-main_i = 0
-char_count = 0
-count = 0
+#Counting will then be performed for the character. Counts are stored in stored_count list. This helps to ensure a 1 to 1 mapping
+#of characters and their respective counts
+
+#Variables
+char = ""               #Stores a single character of the string
+stored_count = []       #Stores the counts of each letter
+found_char = []         #Stores each letter of the entered string
+
+#Loop indexes
+main_i = 0 
+print_i = 0
 
 #Letter count function
-def count_letter(count, char):
+def count_letter(char):
     char_count = 0
     i = 0
 
     while i != len(string):
-        if char in string:
-            char_count += 1
+        if char == string[i]:   #Check if char is equal to the current string index
+            char_count += 1     #Add 1 to the character count if a match occurs
         i += 1
+
     return char_count
 
+#User string input
 string = input("Enter the string: ")
 
-#Loop through the users string and count letter occurrences
+#Main loop. Loop through the users string
 while main_i != len(string):
+    
+    char = string[main_i]   #Get the current letter of the string
 
-    char = string[main_i]
-   
-    #Get the current letter of the string
-    if char in found_char:
+    if char in found_char:  #Check if the letter has already been found
         main_i += 1
         continue
     else:
-        found_char.append(char)
+        found_char.append(char) #If the letter has not yet been found, add it to found char list
     
-    #Count number of occurrences for the letter
-    # char_count = count_letter(count, char)
-    # stored_count.append(char_count)
+    char_count = count_letter(char)     #Count the number of occurrences for the letter
+    stored_count.append(char_count)     #Add the count to the stored_count list
 
     main_i += 1
-print(found_char)
+
+#Display the results of the count
+print()
+for char in found_char:
+    current_char = found_char[print_i]
+    current_count = stored_count[print_i]
+    print(f"Occurrences for {current_char}: {current_count}")
+    print_i += 1
