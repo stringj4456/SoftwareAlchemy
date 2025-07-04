0b3cd6fa2a489b08585d58a2ca8eaf6e53d08161
StingJ <justinsaechao23@gmail.com>
2025-07-04 13:47:48 -0700

    Added menu implementation

--- banksim.py
+++ banksim.py
@@ -4,6 +4,7 @@
 import random
 
 bank_accounts = {}                                   # Store the bank accounts
+quit_prog = False
 
 #Bank account class
 class BankAccount:
@@ -13,23 +14,55 @@ class BankAccount:
         self.number = number                                      # Account number
         self.aid = aid                                            # Account ID
 
+    def deposit(self, deposit):
+        self.balance += amount
+
     def withdraw(self, amount):
         self.balance -= amount
-
 
 
 #Create a new bank account function
 def new_account():
     name = input("Name of the account holder: ")                  # Get the name of the account holder
-    balance = float(input("Initial deposit amount: "))            # Get the accounts intial deposit amount
+    balance = float(input("Initial deposit amount: $"))            # Get the accounts intial deposit amount
     number = random.randint(100000000, 999999999)                 # Generate an account number
     aid = random.randint(1000, 5000)                              # Generate an account ID
 
     print()
     print(f"Congratulations on your new account {name}! Your unique account ID is: {aid}")
+    print()
 
     account = BankAccount(name, balance, number, aid)             # Create the new bank account
     bank_accounts.update({aid: account})                          # Add the new account to bank_accounts dict
 
 
-new_account()
+#Display menu options function
+def menu_display():
+    print("1. Add a new bank account")
+    print("2. Make a deposit")
+    print("3. Make a withdrawal")
+    print("4. Quit the program")
+    print()
+
+
+#Print the welcome message
+print("Welcome to SimBank!")
+print()
+
+#Main program loop
+while quit_prog == False:
+
+    menu_display()
+    menu_option = int(input("Enter a menu option: "))
+    print()
+
+    if menu_option == 1:
+          new_account()
+
+    elif menu_option == 4:
+        print("Thanks for visiting us. We hope to see you again soon")
+        print("Goodbye!")
+        quit_prog = True
+        quit
+
+
