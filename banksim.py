438968f943c79a167f9765f1eba7db6a2ebbd2ba
StingJ <justinsaechao23@gmail.com>
2025-07-03 23:10:17 -0700

    Added create new account function

--- banksim.py
+++ banksim.py
@@ -1,11 +1,24 @@
 #This project is a bank account simulator and will include features such as adding deposits and withdrawing funds. The goal here is to learn to
 #work with classes and get a grasp of OOP.
 
-class BankAccount:
-    def __init__(self, holder):
-        self.holder = holder
+import random
 
-name = input("Enter the name of the account holder: ")
-new_acc = BankAccount(name)
-print(new_acc.holder)
+#Bank account class
+class BankAccount:
+    def __init__(self, name, number, balance):
+        self.name = name 
+        self.number = number
+        self.balance = balance
+
+#Create a new bank account function
+def new_account():
+    name = input("Enter the name of the account holder: ")                  # Get the name of the account holder
+    balance = float(input("Enter the initial deposit amount: "))            # Get the accounts intial deposit amount
+    number = random.randint(100000000, 999999999)                           # Generate an account number
+
+    print()
+    print(f"Congratulations on your new account {name}! Your account number is: {number}")
+    account = BankAccount(name, number, balance)
+
+new_account()
 
